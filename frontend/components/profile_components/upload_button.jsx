fa0dbe96f54e4442b5f83651bb34dff36091a3e6
Roscoe Huo <huoshengdi@gmail.com>
2016-07-06 01:11:28 -0700

    added cloudinary upload button component, not yet integrated

---
+++ frontend/components/profile_components/upload_button.jsx
@@ -0,0 +1,21 @@
+const React = require("react");
+
+const UploadButton = React.createClass({
+  upload: function (e) {
+    e.preventDefault();
+    cloudinary.openUploadWidget(CLOUDINARY_OPTIONS, function(error, results){
+      if(!error){
+        this.props.postImage(results[0]);
+      }
+    }.bind(this));
+  },
+  render: function () {
+    return (
+      <div className="upload-form">
+        <button className="upload-button" onClick={this.upload}>Upload new image!</button>
+      </div>
+    );
+  }
+});
+
+module.exports = UploadButton;
