31811332520d713ff3feb568e373eca1fe5c59ac
%= GitConfig.user_name % <%= GitConfig.user_email %>
2016-07-06 18:30:27 -0700

    fix typo in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -81,16 +81,16 @@ AnswerOption.create!([
   {question_id: 17, body: "Sexist and insulting; women are perfectly capable."}
 ])
 Essay.create!([
-  {user_id: 1, title: "I'm really good at", body: "Cooking things of all kinds"},
   {user_id: 1, title: "My self-summary", body: "Third-year at Bonyari High"},
+  {user_id: 1, title: "I'm really good at", body: "Cooking things of all kinds"},
   {user_id: 1, title: "What I'm doing with my life", body: "Trying to figure out love ;__;"},
   {user_id: 1, title: "I spend a lot of time thinking about", body: "Animals! I love taking care of them. They don't like me very much though..."},
   {user_id: 1, title: "You should message me if", body: "you like animals or sweets\nyou think you can get along well with my dad's goons. They're way too clingy, honestly."},
+  {user_id: 2, title: "My self-summary", body: "I'm in despair!"},
   {user_id: 2, title: "What I'm doing with my life", body: ""},
   {user_id: 2, title: "I'm really good at", body: ""},
   {user_id: 2, title: "I spend a lot of time thinking about", body: ""},
   {user_id: 2, title: "You should message me if", body: ""},
-  {user_id: 2, title: "My self-summary", body: "I'm in despair!"},
   {user_id: 3, title: "My self-summary", body: "Just a girl looking for love ;)"},
   {user_id: 3, title: "What I'm doing with my life", body: "I'm a student at Bonyari High, but I'm planning to go to college when I graduate!"},
   {user_id: 3, title: "I'm really good at", body: "Athletics, math"},
@@ -131,6 +131,6 @@ Question.create!([
 ])
 User.create!([
   {username: "rakuraku15", password_digest: "$2a$10$Om1ZTZTjfbi/3DTMNo/B3.g8ion69G/RNu5vOCaMrNs9vALecXTqa", session_token: "CCPQcAbnuAmVZeZpNdlYLg", avatar_url: "GenkiCupid_Avatars/d3vtyklamxulew3xxplh.jpg", postal_code: "92127", birthdate: "1998-05-15 00:00:00", gender: "male", orientation: "straight", rel_status: "single"},
-  {username: "ItoshikiWish", password_digest: "$2a$10$DDdryKDm4UY/K0phU8w9felP/K0Dnn0c3oJ3xPsDTSEN3.CVHq4my", session_token: "o1S_SdfNW2CfxS_qI-cJ_g", avatar_url: "GenkiCupid_Avatars/teakyle58tqndstu1bvh.jpg", postal_code: "44444", birthdate: "1982-11-04 00:00:00", gender: "male", orientation: "straight", rel_status: "single"}
-  {username: "GoriGurrl", password_digest: "$2a$10$jNaMRcB4//jU50bH88C8WOz6kfTllMkhtXgUvoEY7jIwCgAkYH5pO", session_token: "fLYx7hQ9IGCcPavxsyXa2A", avatar_url: "GenkiCupid_Avatars/pmpct75kf79wzr0wlk6f.png", postal_code: "92127", birthdate: "1998-07-07 00:00:00", gender: "female", orientation: "straight", rel_status: "single"},
+  {username: "ItoshikiWish", password_digest: "$2a$10$DDdryKDm4UY/K0phU8w9felP/K0Dnn0c3oJ3xPsDTSEN3.CVHq4my", session_token: "o1S_SdfNW2CfxS_qI-cJ_g", avatar_url: "GenkiCupid_Avatars/teakyle58tqndstu1bvh.jpg", postal_code: "44444", birthdate: "1982-11-04 00:00:00", gender: "male", orientation: "straight", rel_status: "single"},
+  {username: "GoriGurrl", password_digest: "$2a$10$jNaMRcB4//jU50bH88C8WOz6kfTllMkhtXgUvoEY7jIwCgAkYH5pO", session_token: "fLYx7hQ9IGCcPavxsyXa2A", avatar_url: "GenkiCupid_Avatars/pmpct75kf79wzr0wlk6f.png", postal_code: "92127", birthdate: "1998-07-07 00:00:00", gender: "female", orientation: "straight", rel_status: "single"}
 ])
